99119c8324fb69abb28f37f96a70bcdcbf9c5d0a
SebastianRojas748 <78050856+SebastianRojas748@users.noreply.github.com>
2021-03-04 11:35:35 -0800

    2

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ Educational dinosaur arena
 
 ### How it works
 read instructions
+use #1-5 to start with any dino and begin experience
+keep switching through dinos with same keys and reading facts about each dino.
+switching dinos means that the previous dino was eaten, playing an audio cue.
+once 50 seconds pass, the meteor will strike the screen and game will end.
--- sketch.js
+++ sketch.js
@@ -7,6 +7,8 @@
 // variable that is a function 
 var drawFunction;
 
+
+
 // rounded corner variable
 var crnr = 8;
 
@@ -17,45 +19,46 @@ var raptorAssets = [];
 var carnoAssets = [];
 var baryAssets = [];
 var mainAssets = [];
+var meteorAssets = [];
 
 
 //room color array
 var roomColors = [];
 
 //navigation instruction bar variables
-var xNav = 500;
+var xNav = 1400;
 var yNav = 11;
 var hNav = 40;
-var wNav = 240;
+var wNav = 300;
 var navFill = 255;
 
-var navTextXPos = (xNav + (740 - xNav)/2);
+var navTextXPos = (xNav + (1690 - xNav)/2);
 var navTextYPos = (yNav + (hNav/2) + 9);
 var navTextSize = 26;
 var strings = ['use keys to switch dinos'];
 
 //bounding box navigation key  variables
 var bnd = 40;
-var xKyBnd = 749;  //postion 1 on bounding box
+var xKyBnd = 1749;  //postion 1 on bounding box
 var yKyBnd = 11;  //postion 1 on bounding box
 var bndSpcr = 51; //spacer between keys
 
 //navigation key placement variables
-var xKeyPlace = (xKyBnd + (789 - xKyBnd)/2);
+var xKeyPlace = (xKyBnd + (1789 - xKyBnd)/2);
 var yKeyPlace = (yKyBnd + (bnd/2) + 7);
 
 //navigation key array
 var navKey = [];
 
 //dino name bounding box variables
-var xStartOne = 604;
-var yStartOne = 543;
-var xEndOne = 185;
+var xStartOne = 870;
+var yStartOne = 11;
+var xEndOne = 300;
 var yEndOne = 40;
 var titleFill = 0;
 
 //dino name variables
-var textXPos = (xStartOne + (789 - xStartOne)/2);
+var textXPos = (xStartOne + (1139 - xStartOne)/2);
 var textYPos = (yStartOne + (yEndOne/2) + 9);
 
 //preload images in array
@@ -80,15 +83,30 @@ function preload() {
 
   //Garden images
   mainAssets[0] = loadImage('assets/background.png');
+  mainAssets[1] = loadImage('assets/logo.png');
+
+  //meteor
+  meteorAssets[0] = loadImage('assets/explosion.png');
+
+  //chomp
+  chomp = loadSound('assets/chomp.mp3');
 
 }
 
+ let chomp;
+ let timer;
+ let interval = 50000;
+ let countdown;
+ let state = 0;
+
 // Center drawing, drawFunction will be one for default
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(windowWidth, windowHeight);
   textAlign(CENTER);
   textSize(28);
   noStroke();
+  timer = millis() + interval;
+
 
   //seting the room color array
   roomColors[0] = color(204, 0, 0); 
@@ -113,6 +131,21 @@ function setup() {
 //calls state machine function
 function draw() {
   drawFunction();
+
+    countdown = ceil((timer-millis())/1000);
+  
+  if (state == 0) {
+    text(countdown, 100, 100);
+  }
+   else if (state == 1) {
+     image(meteorAssets[0], 400, 15);
+  }
+
+  if (timer < millis()) {
+    timer = millis() + interval;
+    state = 1;
+  }
+
 }
 
 //draws images from main array
@@ -120,7 +153,8 @@ drawmain = function() {
    background(roomColors[0]);
 
    //images in array
-   image(mainAssets[0], 0, 0);    
+   image(mainAssets[0], 0, 0);
+   image(mainAssets[1], width/2-250, height/3);    
    
    //text bounding box
    fill(roomColors[0]);
@@ -182,6 +216,8 @@ drawmain = function() {
    noStroke();
    textSize(navTextSize);
    text(strings[0], navTextXPos, navTextYPos); 
+
+   
 }
 
 //draws images from trex array
@@ -189,12 +225,19 @@ drawtrex = function() {
   background(roomColors[1]);
 
    //images in array
-   image(trexAssets[0], mouseX, mouseY);  
+   image(mainAssets[0], 0, 0);
+   image(trexAssets[0], mouseX-350, mouseY-100);  
    
    
    //text bounding box
    fill(roomColors[1]);
    rect(xStartOne, yStartOne, xEndOne, yEndOne, crnr);
+
+
+   //box2
+   rect(500,800,800,150,crnr);
+   fill(0);
+   text('Tyrannosaurus rex, or “Tyrant Lizard”, one of the largest dinosaurs to have walked the earth, lived 70 million years ago in what is now North America.', 500, 850, 800);
 
    //room title
    fill(titleFill);
@@ -250,11 +293,17 @@ drawspino = function() {
 
   
    //images in array
-   image(spinoAssets[0], mouseX, mouseY); 
+   image(mainAssets[0], 0, 0);
+   image(spinoAssets[0], mouseX-400, mouseY-100); 
    
    //text bounding box
    fill(roomColors[2]);
    rect(xStartOne, yStartOne, xEndOne, yEndOne, crnr);
+
+   //box2
+   rect(500,800,800,150,crnr);
+   fill(0);
+   text('Spinosaurus, which was longer and heavier than Tyrannosaurus, is the largest known carnivorous dinosaur.', 500, 850, 800);
 
    //room title
    fill(titleFill);
@@ -310,17 +359,24 @@ drawraptor = function() {
 
 
    //images in array
-   image(raptorAssets[0], mouseX, mouseY);  
-  
-  
-  
+   image(mainAssets[0], 0, 0);
+   image(raptorAssets[0], mouseX-150, mouseY-100);  
+
+   
    //text bounding box
    fill(roomColors[3]);
    rect(xStartOne, yStartOne, xEndOne, yEndOne, crnr);
 
+   //box2
+   rect(500,800,800,150,crnr);
+   fill(0);
+   text('The name on this dinosaur, which is derived from the Latin words "velox" (swift) and "raptor" (robber or plunderer), as an apt description of its agility and carnivorous diet.', 500, 850, 800);
+
+
    //room title
    fill(titleFill);
    text('Velociraptor', textXPos, textYPos);
+
 
       //nav keys
    //bounding position one
@@ -371,12 +427,19 @@ drawcarno = function() {
    background(roomColors[4]);
 
    //images in array
-   image(carnoAssets[0], mouseX, mouseY); 
+   image(mainAssets[0], 0, 0);
+   image(carnoAssets[0], mouseX-300, mouseY-100); 
   
   
    //text bounding box
    fill(roomColors[4]);
    rect(xStartOne, yStartOne, xEndOne, yEndOne, crnr);
+
+    //box2
+   rect(500,800,800,150,crnr);
+   fill(0);
+   text('The name Carnotaurus means ‘meat eating bull’….eek! The name refers to the horns found above its eyes, which are similar to bull horns.', 500, 850, 800);
+
 
    //room title
    fill(titleFill);
@@ -432,13 +495,20 @@ drawbary = function() {
 
 
    //images in array
-   image(baryAssets[0], mouseX, mouseY);  
+   image(mainAssets[0], 0, 0);
+   image(baryAssets[0], mouseX-250, mouseY-100);  
 
   
   
    //text bounding box
    fill(roomColors[5]);
    rect(xStartOne, yStartOne, xEndOne, yEndOne, crnr);
+
+   //box2
+   rect(500,800,800,150,crnr);
+   fill(0);
+   text('Not surprisingly, Baryonyx was named in reference to that prominent claw--which, however, had nothing to do with the prominent claws of another family of carnivorous dinosaurs, the Raptors.', 500, 850, 800);
+
 
    //room title
    fill(titleFill);
@@ -489,9 +559,15 @@ drawbary = function() {
 }
 
 function keyPressed() {
-  // shows which was types
-  //print(key);
-  print(keyCode);
+  if (chomp.isPlaying()) {
+    chomp.stop()
+    }
+    else {
+     chomp.play();
+    }
+
+  
+   
 
    // main menu
   if( drawFunction === drawmain ) {
